4c6dd9e628ffffdfae194248f627e53324152653
John D. Corbett <corbett@pictographer.com>
2024-12-22 17:02:01 -0800

    Remove include_str! macro and remove assumption that version file will be present

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::include_str;
-
 fn main() {
     println!("Hello, new world!");
     println!(
         "version from build env: {}",
         option_env!("GITHUB_REF").unwrap_or("GITHUB_REF unset")
     );
-    println!("version from file: {}", include_str!("../version"));
+}
+
+#[test]
+fn test_trivial_example() {
+    assert_eq!(true, true);
 }
